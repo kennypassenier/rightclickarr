31cfaaea57247ab3ac9d6ab5048b7948bff9d33e
Kenny Passenier <kennypassenier@gmail.com>
2022-08-31 19:00:02 +0200

    Updated to v0.4.0

--- background.js
+++ background.js
@@ -2,35 +2,45 @@
 // We use it to init the objects we will be using from localStorage
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({
-    options: [
-      {
-        id: "1",
-        name: "Add to movies",
-        url: "http://localhost:7878/add/new?newItem="
-      },
-      {
-        id: "2",
-        name: "Add to TV series",
-        url: "http://localhost:8989/add/new?newItem="
-      }, 
-      {
-        id: "3",
-        name: "Search Youtube", 
-        url: "https://www.youtube.com/results?search_query="
-      }
-    ],
+    options: {
+      items: [
+        {
+          id: "1",
+          name: "Add to movies",
+          url: "http://localhost:7878/add/new/"
+        },
+        {
+          id: "2",
+          name: "Add to TV series",
+          url: "http://localhost:8989/add/new/"
+        }, 
+        {
+          id: "3",
+          name: "Search Youtube", 
+          url: "https://www.youtube.com/results?search_query="
+        }, 
+        {
+          id: "4",
+          name: "Search IMDB", 
+          url: "https://www.google.com/search?q=imdb+"
+        }
+      ],
+      idCounter: 3, // Make sure that this always corresponds with the amount of provided options
+    }
   });
 });
 
-let options = [];
+let options;
 chrome.storage.local.get("options", data => {
   options = data.options;
 });
 
 
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Listen to the getOptions message
   if(request.message === "getOptions"){
+    console.log("get options");
     // Return the relevant data
     chrome.storage.local.get("options", data => {
       // Check for errors
@@ -50,12 +60,33 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     })
     return true;
   }
+  else if(request.message = "setOptions"){
+    console.log("set options");
+    console.log(request.payload)
+    chrome.storage.local.set({
+      "options": request.payload,
+    });
+    if(chrome.runtime.lastError){
+      sendResponse({
+        message: "failed",
+      })
+      return true
+    }
+    // No errors
+    sendResponse({
+      message: "success",
+    })
+    // Update context menu
+    options = request.payload;
+    createContextItems();
+  }
+  return true;
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info) => {
   console.log(info);
   // Get the item from options that corresponds with the id of the contextmenu item
-  const option = options.find((option) => {
+  const option = options.items.find((option) => {
     return option.id === info.menuItemId;
   });
   console.log(option);
@@ -72,6 +103,9 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 
 function createContextItems(){
   console.log('Creating context items');
+  // Remove any that have been previously made so we can update properly
+  chrome.contextMenus.removeAll();
+
   // Create context menu items
   chrome.storage.local.get("options", data => {
     // Check for errors
@@ -79,10 +113,10 @@ function createContextItems(){
       console.log("There was an error");
     }
     // No errors
-    for(let option of data.options){
+    for(let item of data.options.items){
       const obj = {
-        id: option.id,
-        title: option.name,
+        id: item.id,
+        title: item.name,
         contexts: ["selection"],
       }
       chrome.contextMenus.create(obj);  
--- optionsScript.js
+++ optionsScript.js
@@ -1,1 +1,159 @@
+let options;
+
 // Allow the user to add options and save those to localstorage
+chrome.runtime.sendMessage({message: "getOptions"}, response => {
+  if(response.message === "success"){
+    options = response.payload;
+    updateTable();
+  }
+})
+
+function saveOptions(){
+  chrome.runtime.sendMessage({
+    message: "setOptions",
+    payload: options,
+  }, response => {
+    if(response.message === "success"){
+      console.log("Options have been succesfully saved");
+      updateTable();
+    }
+  });
+}
+
+
+function updateTable(){
+  let {items} = options;
+  let tableBody = document.querySelector("#optionsTableBody");
+  // Reset table
+  tableBody.innerHTML = ""
+  for(item of items){
+    // Create elements
+    const row = document.createElement("tr");
+
+    const itemTextData = document.createElement("td");
+    const itemUrlData = document.createElement("td");
+    const saveButtonData = document.createElement("td");
+    const deleteButtonData = document.createElement("td");
+
+
+    const itemText = document.createElement("input");
+    const itemUrl = document.createElement("input");
+    const saveButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    // Fill elements with relevant data
+    row.setAttribute("data-id", item.id);
+
+    itemText.value = item.name;
+    itemUrl.value = item.url;
+
+    saveButton.textContent = "Save";
+    deleteButton.textContent = "Delete";
+
+    // Event listeners for the buttons
+    saveButton.addEventListener("click", (e) => {
+      // Select relevant elements
+      const parent = e.target.parentElement.parentElement;
+      const firstInput = parent.querySelector("input");
+      const secondInput = firstInput.parentElement.nextSibling.querySelector("input");
+      const id = "" + parent.dataset.id;
+      // Get the item we want to change
+      const itemToUpdate = options.items.find((item) => {
+        return item.id === id;
+      });
+      console.log(options);
+      console.log(id);
+      console.log(parent);
+      console.log(firstInput);
+      console.log(secondInput);
+      console.log(itemToUpdate);
+      
+      if(itemToUpdate){
+        // Change the data
+        itemToUpdate.name = firstInput.value;
+        itemToUpdate.url = secondInput.value;
+        // Save the date
+        saveOptions();
+      }
+      else{
+        console.log("item not found");
+      }
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      const id = e.target.parentElement.parentElement.dataset.id;
+      // Filter out the items with the corresponding id
+      options.items = options.items.filter((item) => {
+        return item.id !== id;
+      });
+      saveOptions();
+    });
+
+    // Add elements to each other and the table
+    itemTextData.appendChild(itemText);
+    itemUrlData.appendChild(itemUrl);
+
+    saveButtonData.appendChild(saveButton);
+    deleteButtonData.appendChild(deleteButton);
+
+    row.appendChild(itemTextData);
+    row.appendChild(itemUrlData);
+    row.appendChild(saveButtonData);
+    row.appendChild(deleteButtonData);
+
+    tableBody.appendChild(row);    
+  }
+  // Append row for new items
+  tableBody.appendChild(createNewItemRow());
+}
+
+function createNewItemRow(){
+  // Returns a new row where we can create new items
+  let {idCounter} = options;
+  // idCounter holds the latest used id
+
+  // Create elements
+  const newRow = document.createElement("tr");
+
+  const itemTextData = document.createElement("td");
+  const itemUrlData = document.createElement("td");
+  const addButtonData = document.createElement("td");
+  
+  const itemText = document.createElement("input");
+  const itemUrl = document.createElement("input");
+  const addButton = document.createElement("button");
+
+  // Add info the the elements
+
+  newRow.setAttribute("data-id", idCounter + 1);
+  addButton.textContent = "Add item";
+
+  itemTextData.appendChild(itemText);
+  itemUrlData.appendChild(itemUrl);
+
+  addButtonData.appendChild(addButton);
+
+  newRow.appendChild(itemTextData);
+  newRow.appendChild(itemUrlData);
+  newRow.appendChild(addButtonData);
+
+  // Event listener
+  addButton.addEventListener("click", (e) => {
+    // Select relevant elements
+    const parent = e.target.parentElement.parentElement;
+    const firstInput = parent.querySelector("input");
+    const secondInput = firstInput.parentElement.nextSibling.querySelector("input");
+    const id = "" + parent.dataset.id;
+
+    let newItem = {
+      id,
+      name: firstInput.value,
+      url: secondInput.value,
+    }
+    options.items.push(newItem);
+    options.idCounter++;
+    saveOptions();
+  })
+
+  return newRow;
+
+}
